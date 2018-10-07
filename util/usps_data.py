7649fb5da3c2a450c99eed01df4a45b8c3a54979
signapoop <leowyaoyang@signapoop.asia-east1-b.c.geometric-progression.internal>
2018-10-07 19:06:05 +0000

    jupyter

--- util/usps_data.py
+++ util/usps_data.py
@@ -6,22 +6,23 @@ from torchvision import transforms
 
 # The dataset has 7291 train and 2007 test images. The images are 16x16 grayscale pixels.
 
-def draw_random_usps_samples(data_dir, n_samples, train=True):
+def draw_random_usps_samples(data_dir, n_samples=None, train=True):
     with h5py.File(data_dir, 'r') as hf:
 
         if train:
             train = hf.get('train')
             X = train.get('data')[:]
-            y = train.get('target')[:]
+            labels = train.get('target')[:]
 
         else:
             test = hf.get('test')
             X = test.get('data')[:]
-            y = test.get('target')[:]
-
-    random_idx = np.random.choice(list(range(X.shape[0])), n_samples, replace=False)
-    X = X[random_idx, :]
-    labels = y[random_idx]
+            labels = test.get('target')[:]
+    
+    if n_samples is not None:
+        random_idx = np.random.choice(list(range(X.shape[0])), n_samples, replace=False)
+        X = X[random_idx, :]
+        labels = labels[random_idx]
 
     X = X.reshape(-1, 1, 16, 16).transpose((0, 2, 3, 1))  # convert to HWC
 
